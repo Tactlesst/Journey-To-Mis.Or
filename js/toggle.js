e252a270d48b5cf3f67be274567f4501f22bf41c
Tactlesst <nasefaquiatan@gmail.com>
2024-10-31 01:57:32 +0800

    version 3.0
    Biggest version update data more

--- js/toggle.js
+++ js/toggle.js
@@ -1,9 +1,27 @@
 map.on('click', function() {
-    map.scrollWheelZoom.enable();
-  });
-  document.addEventListener('click', function(event) {
-    const mapContainer = document.getElementById('map-container');
-    if (!mapContainer.contains(event.target)) {
-      map.scrollWheelZoom.disable();
+  map.scrollWheelZoom.enable();
+});
+document.addEventListener('click', function(event) {
+  const mapContainer = document.getElementById('map-container');
+  if (!mapContainer.contains(event.target)) {
+    map.scrollWheelZoom.disable();
+  }
+  
+});
+// Disable dragging initially
+map.dragging.disable();
+
+const selectElement = document.getElementById('subjects');
+const imagePreview = document.getElementById('image-preview');
+
+selectElement.addEventListener('change', function() {
+    const selectedOption = this.options[this.selectedIndex];
+    
+    if (selectedOption.value) {
+        const imageUrl = selectedOption.getAttribute('data-image');
+        imagePreview.src = imageUrl;
+        imagePreview.style.display = 'block'; // Show the image
+    } else {
+        imagePreview.style.display = 'none'; // Hide the image if no location is selected
     }
-  });
+});
